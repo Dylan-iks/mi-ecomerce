898ef859e2281e5ba4876522eb109f37dbb75308
Dylan Medina <dylanmedina54@gmail.com>
2024-01-23 23:25:38 -0300

    terminando de agregar context

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,17 @@ import ItemListContainer from './components/cards/ItemListContainer'
 import ItemDetailContainer from './components/cards/ItemDetailContainer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Contacto from './components/contacto/Contacto'
-import { CartContext } from './components/context/CartContext'
-import { useState } from 'react'
+import { CartProvider } from './components/context/CartContext'
 import Carrito from './components/carrito/Carrito'
 
 
 
 function App() {
 
-  const [carrito, setCarrito] = useState([])
-
-  const agregarAlCarrito = (item, cantidad) => {
-      
-    const itemAgregado = {...item, cantidad};
-
-    const nuevoCarrito = [...carrito]
-
-    const estaEnElCarrito = nuevoCarrito.find((producto) => producto.id === itemAgregado.id)
-    
-    if (estaEnElCarrito){
-      estaEnElCarrito.cantidad += cantidad
-    } else {
-      nuevoCarrito.push(itemAgregado)
-    }
-
-    setCarrito(nuevoCarrito)  
-  
-  }
-  
-  const cantidadEnCarrito = () => {
-    return carrito.reduce((acc, prod) => acc + prod.cantidad, 0)
-  }
-
-  const precioTotal = () => {
-    return carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad)
-  }
 
   return (
     <>
-      <CartContext.Provider value={ {carrito, agregarAlCarrito, cantidadEnCarrito} }>
+      <CartProvider>
         <BrowserRouter>
             
           <NavBar/>
@@ -60,8 +32,8 @@ function App() {
 
             
           
-          </BrowserRouter> 
-      </CartContext.Provider> 
+        </BrowserRouter> 
+      </CartProvider>
       
       {/* <ItemCount stock = {10}/> */}
     </>
